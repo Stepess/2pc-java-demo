2c7ed64c2525c65b56ea0ad5f0ccecb5a401d0b2
Stepess <stepanersh@gmail.com>
2020-02-14 09:30:04 +0200

    add sb application

---
+++ src/main/java/ua/stepess/microservices/pcdemo/Application.java
@@ -0,0 +1,13 @@
+package ua.stepess.microservices.pcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
