497d50e089bc9487bfff569b29273283a3aca63c
Stepess <stepanersh@gmail.com>
2020-02-14 09:57:17 +0200

    add more data

--- init-db/fly/data.sql
+++ init-db/fly/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO fly_booking VALUES (1, 'Stepan', 'KLM 1382', 'KBP', 'AMS', '01/05/2015');
+INSERT INTO fly_booking VALUES
+(1, 'Stepan', 'KLM 1382', 'KBP', 'AMS', '01/05/2015'),
+(2, 'Alex', 'ATX 1984', 'KBP', 'AMS', '03/04/2020'),
+(3, 'Lua', 'CTC 1917', 'AVR', 'TMC', '05/07/2020');
